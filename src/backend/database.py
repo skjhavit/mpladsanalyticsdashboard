9a9bea96e1bcaee9ff16edbe7a4bd4fef7b01ef8
skjhavit <skjhavit@gmail.com>
2026-01-16 08:50:22 -0600

    db add

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 import os
 
-DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/govwork.db"))
+DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "data/govwork.db"))
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 # check_same_thread=False is needed for SQLite with multi-threaded web servers
